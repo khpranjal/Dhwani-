e8b095bb6ab756118456cb19fa66bae97f79ea97
khpranjal <30779111+khpranjal@users.noreply.github.com>
2018-11-08 22:38:12 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,300 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+	
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+    <title>Dhwani</title>
+	<link rel="icon" href="image/mic2.png" type="image/png">
+	<style>
+	.navbar{
+		background-color:#C82333;
+		text-align:center;
+		color:white;
+		
+	}
+	
+	
+	.btn1{
+		
+		background-color: #FFFFFF;
+		color:#C82333;
+		border-radius:20px;
+		border:1px #5E5C5C solid;
+		font-weight: 500;
+		width:120px;
+		
+	}
+	.btn2{
+		
+
+
+		border-radius:30%
+
+	
+		
+	}
+	#search{
+	  width:335px;
+	  border-radius:20px;
+	  padding:1px 0px 3px 20px;
+	  border:1px #5E5C5C solid;
+	  font-weight: 550px;
+	}
+	
+	
+	#navbar2{
+    	        height:50px;
+	
+	
+	}
+	
+	.jumbotron {
+						background-image: url(image/3.jpg);
+						text-align:center;
+						color:black;
+						width:100%;
+						height:580px;
+						color:white;
+					    background-size:cover;
+		   
+		     			}
+
+	.product{
+			width:100%;
+		    height:522px;
+		}
+		
+		.lang{
+			width:100%;
+		    height:80%;
+		}
+		
+		
+    .discover{
+		
+	       margin-top:40px;
+		   margin-bottom:80px;
+		
+		
+	}		
+ #jumbo{
+	 background-color:white;
+	 opacity:0.7;
+	 color:black;
+	 border-radius:20px;
+	 height:430px;
+   }
+
+   .card:hover{
+    -webkit-box-shadow: -1px 9px 40px -12px rgba(0,0,0,0.75);
+    -moz-box-shadow: -1px 9px 40px -12px rgba(0,0,0,0.75);
+    box-shadow: -1px 9px 40px -12px rgba(0, 0, 0, 0.75);
+}
+
+ #wrapper{
+			  
+			  padding-top:15px;
+			  border-top:0.5px black dashed;
+		  }
+		  
+	
+	</style>
+	
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-md navbar-dark">
+  <a class="navbar-brand" href="#"><img src="image/mic4.png"><i><b>  Dhwani</b></i></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><h6>Home</h6> <span class="sr-only">(current)</span></a>
+      </li> 
+        <li class="nav-item active">
+        <a class="nav-link" href="login.html"><h6>Sign in</h6><span class="sr-only">(current)</span></a>
+        </li>
+       <li class="nav-item active">
+        <a class="nav-link" href="user.php"><h6>My account</h6> <span class="sr-only">(current)</span></a>
+      </li> 
+        
+    </ul>
+   <form class="form-inline">
+    <input class="form-control mr-lg-3 " type="search" placeholder="Search for songs and more." aria-label="Search" id="search">
+    <a class="btn btn-outline-danger btn1" href="" role="button"><i class="fas fa-search"></i>    Search</a>
+  </form>
+  </div>
+</nav>
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <div id="jumbo">  
+    <h1 class="display-4"><b>Dhwani</b></h1>
+    <p class="lead"></p>
+  </div>
+  </div>
+  </div>
+
+
+	<div class="container-fluid">
+         <div class="col-sm-12 text-center" id="summary">
+            <h1><u>Marathi</u></h1>
+          </div>
+    </div>
+
+<div class="container discover ">
+<div class="card-deck">
+
+
+  <div class="card">
+    <img class="card-img-top lang" src="image/mrathi.png" alt="Card image cap">
+    <div class="card-body">
+      <a href="Marathi1.html">Marathi</a>
+    </div>
+  </div>
+
+  
+
+   <div class="card">
+    <img class="card-img-top lang" src="image/mrathi.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Marathi</h5>
+    </div>
+  </div>
+ 
+  
+
+    <div class="card">
+    <img class="card-img-top lang" src="image/mrathi.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Marathi</h5>
+    </div>
+  </div>
+
+ 
+   <div class="card">
+    <img class="card-img-top lang" src="image/mrathi.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Marathi</h5>
+    </div>
+  </div>
+ 
+</div>
+
+	<div class="container-fluid">
+         <div class="col-sm-12 text-center" id="summary" style="margin-top:30px;margin-bottom:30px;">
+            <h1><u>Hindi</u></h1>
+          </div>
+    </div>
+	<div class="card-deck">
+      <div class="card">
+    <img class="card-img-top lang" src="image/hindi.jpg" alt="Card image cap">
+    <div class="card-body">
+        <a href="Hindi1.html">Hindi kavitaye</a>
+
+    </div>
+  </div>
+  
+    <div class="card">
+    <img class="card-img-top lang" src="image/hindi.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Hindi</h5>
+
+    </div>
+  </div>
+  
+    <div class="card">
+    <img class="card-img-top lang" src="image/hindi.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Hindi</h5>
+     </div>
+    </div>
+	
+	 <div class="card">
+    <img class="card-img-top lang" src="image/hindi.jpg" alt="Card image cap">
+     <div class="card-body">
+      <h5 class="card-title">Hindi</h5>
+     </div>
+    </div>
+	
+  </div>
+</div>
+
+
+
+	<div class="container-fluid" style="margin-bottom:30px;">
+         <div class="col-sm-12 text-center" id="summary" style="margin-top:30px;margin-bottom:30px;">
+            <h3><u>Follow us</u></h3>
+          </div>
+		    <div class="row justify-content-center">
+			<div class="text-center">
+			<div class="col-sm-4">
+		  	<div style="font-size: 48px;padding:4px 0px 4px 0px"><a href="https://www.facebook.com/romitlimje7" Target="_blank"><i class="fab fa-facebook-square fa-lg" style="color:#C82333"></i></a></div>
+            </div>
+			</div>
+			<div class="col-sm-4">
+			<div class="text-center">
+			<div style="font-size: 48px; color: black; padding:4px 0px 4px 0px"><a href="https://www.instagram.com/romitlimje7/ " Target="_blank"><i class="fab fa-instagram fa-lg" style="color:#C82333"></i></a></div>
+			</div>
+			</div>
+			<div class="text-center">
+			<div class="col-sm-4">
+			<div style="font-size: 48px; padding:4px 0px 4px 0px"><a href=" " Target="_blank"><i class="fab fa-twitter-square fa-lg" style="color:#C82333"></i></a></div>
+           </div>
+		   </div>
+		  </div>
+		  
+    </div>
+
+      <div class="container-fluid" id="wrapper" style="margin-bottom:50px;">
+        <div class="row" id="wrapper1">
+              <div class="col-lg-6 col-md-12 text-secondary text-center text-lg-left">
+                    <p class="pb-2">Copyright &copy; 2018 <a href="http://romitl-in.stackstaging.com/" target="_blank">Dhwani.in</a> </p>
+               </div>
+			   
+
+               <div class="col-lg-6 col-md-12 text-secondary text-center text-lg-right">
+                    <ul class="list-inline pb-2">
+                         <li class="list-inline-item">
+                            <a class="py-2 text-secondary" href="Returnpolicy.html" target="_blank">Privacy Policy</a> |
+							<a class="py-2 text-secondary" href="terms.html" target="_blank">Terms of Use</a> 
+							
+                          </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
+
+<nav class="nav nav-pills nav-fill navbar fixed-bottom navbar-light " id="navbar2">
+  <div style="margin-left:60px;"><a class="btn btn-outline-light btn2" href="#" role="button" ><i class="fas fa-home fa-1x"></i></a></div>
+  <div class="setting"><a class="btn btn-outline-light btn2" href="" role="button"><i class="fas fa-headphones-alt"></i></a></div>
+  <div class="setting" style="margin-right:60px;"><a class="btn btn-outline-light btn2" href="user.php" role="button"><i class="fas fa-user-alt fa-1x"></i></a></div>
+</nav>
+    
+
+
+
+
+
+
+	
+	
+	
+	
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
